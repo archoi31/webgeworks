a56ce7a7785dfa5a74d9e957dc4a213b411381e6
archoi31 <archoi3107@gmail.com>
2023-06-19 17:45:57 +0900

    shaft page complete

--- js/lib/choose-color.js
+++ js/lib/choose-color.js
@@ -84,10 +84,10 @@ $.fn.sbxColorChoice = function (params) {
     element.css({ 'top': top, 'left': left }).addClass([elementoPosicao, elementoPosicao2]);
   }
 
-  /*Event click*/
+  /*Event click
   $(this).click(function (e) {
     if (!openChooseColor) {
-      $.get("choose-color.html", function (data) {
+      $.get("./choose-color.html", function (data) {
         const $element = $(data);
 
         $('body').append($element);
@@ -105,7 +105,48 @@ $.fn.sbxColorChoice = function (params) {
       });
     }
 
+  });*/
+
+  $(this).click(function (e) {
+    if (!openChooseColor) {
+
+      data = 
+      `<div class="co-color-list co-shadow-3dp" style="display:none" tabindex="-1">
+        <h1>Pick A Color</h1>
+        <ul class="color-pallet">
+          <li class="co-hue-1" data-color="#000000" data-name="BV Black" style="background-color:#000000"></li>
+          <li class="co-hue-1" data-color="#ffffff" data-name="White Out" style="background-color:#FFFFFF"></li>
+          <li class="co-hue-1" data-color="#f3bcd4" data-name="Pink Ribbon" style="background-color:#f3bcd4"></li>
+          <li class="co-hue-1" data-color="#990000" data-name="TVD Red" style="background-color:#990000"></li>
+      
+          <li class="co-hue-1" data-color="#FF7200" data-name="Cowboy Orange" style="background-color:#FF7200"></li>
+          <li class="co-hue-1" data-color="#FCE000" data-name="Cerveza Yellow" style="background-color:#FCE000"></li>
+          <li class="co-hue-1" data-color="#5ADD45" data-name="Key Lime" style="background-color:#5ADD45"></li>
+          <li class="co-hue-1" data-color="#00A844" data-name="Irish Green" style="background-color:#00A844"></li>
+          
+          <li class="co-hue-1" data-color="#8bb4e8" data-name="Carolina Blue" style="background-color:#8bb4e8"></li>
+          <li class="co-hue-1" data-color="#0019a8" data-name="Oceanside Blue" style="background-color:#0019a8"></li>
+          <li class="co-hue-1" data-color="#4f03a9" data-name="Purple Haze" style="background-color:#4f03a9"></li>
+        </ul>
+      </div>`;
+    
+      const $element = $(data);
+
+        $('body').append($element);
+        $('body').css('overflow', 'hidden');
+
+        positionElement($element, e)
+        effectModal($element)
+        chooseColor($element, e)
+        removePalletColor()
+        constumResetTextButton($element)
+
+        $element.focusout(() => closeChooseColor($element));
+
+        openChooseColor = true;
+    }
   });
+
 }
 
 
@@ -191,7 +232,7 @@ $.fn.sbxMixColorChoice = function (params) {
     element.css({ 'top': top, 'left': left }).addClass([elementoPosicao, elementoPosicao2]);
   }
 
-  /*Event click*/
+  /*Event click
   $(this).click(function (e) {
     if (!openChooseColor) {
       $.get("choose-mix-color.html", function (data) {
@@ -212,5 +253,57 @@ $.fn.sbxMixColorChoice = function (params) {
       });
     }
 
+  });*/
+
+  $(this).click(function (e) {
+    if (!openChooseColor) {
+
+      data =               
+      `<div class="co-color-list co-shadow-3dp" style="display:none" tabindex="-1">
+      <h1>Pick A Color</h1>
+      <ul class="color-pallet mix">
+        <li class="co-hue-1" data-color1="#000000" data-color2="#FFFFFF" data-name="Black/White" >
+          <div class="half-color" style="background-color:#000000"></div>
+          <div class="half-color" style="background-color:#FFFFFF"></div>
+        </li>
+        <li class="co-hue-1" data-color1="#FFFFFF" data-color2="#000000" data-name="White/Black" >
+          <div class="half-color" style="background-color:#FFFFFF"></div>
+          <div class="half-color" style="background-color:#000000"></div>
+        </li>
+        <li class="co-hue-1" data-color1="#847248" data-color2="#000000" data-name="Gold/Black" >
+          <div class="half-color" style="background-color:#847248"></div>
+          <div class="half-color" style="background-color:#000000"></div>
+        </li>
+        <li class="co-hue-1" data-color1="#FFFFFF" data-color2="#FFFFFF" data-name="White/White" >
+          <div class="half-color" style="background-color:#FFFFFF"></div>
+          <div class="half-color" style="background-color:#FFFFFF"></div>
+        </li>
+        <li class="co-hue-1" data-color1="#000000" data-color2="#000000" data-name="Black/Black" >
+          <div class="half-color" style="background-color:#000000"></div>
+          <div class="half-color" style="background-color:#000000"></div>
+        </li>
+        <li class="co-hue-1" data-color1="#C0C0C0" data-color2="#000000" data-name="Silver/Black" >
+          <div class="half-color" style="background-color:#C0C0C0"></div>
+          <div class="half-color" style="background-color:#000000"></div>
+        </li>
+      </ul>
+      </div>`;
+    
+      const $element = $(data);
+
+        $('body').append($element);
+        $('body').css('overflow', 'hidden');
+
+        positionElement($element, e)
+        effectModal($element)
+        chooseColor($element, e)
+        removePalletColor()
+        constumResetTextButton($element)
+
+        $element.focusout(() => closeChooseColor($element));
+
+        openChooseColor = true;
+    }
   });
 }
+
